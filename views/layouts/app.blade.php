49aa1c12fcdcf312a0f4e200af72df7be6c666e3
Emeka Elo-Chukwuma <elochux@gmail.com>
2020-08-01 20:21:13 +0100

    laravel blade templating
    laravel blade templating of the upgrade accounts page

---
+++ views/layouts/app.blade.php
@@ -0,0 +1,28 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+   <!-- Required meta tags -->
+   <meta charset="utf-8">
+   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+   <title>{{config('app.name', 'Upgrade Account')}}</title>
+   <!-- Bootstrap CSS -->
+   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css"
+      integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+   <link id="link-id" rel="stylesheet" href="assets/uacss/nav.css">
+   <link rel="stylesheet" href="assets/uacss/inner-nav.css">
+   <link rel="stylesheet" type="text/css" href="assets/uacss/style.css">
+   <!--bootstrap-select jQuery plugin css-->
+   <link rel="stylesheet"
+      href="https://cdn.jsdelivr.net/npm/bootstrap-select@1.13.14/dist/css/bootstrap-select.min.css">
+
+
+
+</head>
+
+<body class="collapse-sidebar">
+
+   @yield('content')
+</body>
+
+</html>
